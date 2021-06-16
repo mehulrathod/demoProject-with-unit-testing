300bf23351109c8fbb08ca394b02fc5c98f53f93
mehul rathod <mehulrathod@mehuls-Mac-mini.local>
2021-06-17 01:20:28 +0530

    updated unit testing

--- services/api/v1/OrderService_test.go
+++ services/api/v1/OrderService_test.go
@@ -2,13 +2,10 @@ package v1Service
 
 import (
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"testing"
-	"time"
 	"vipulsirdemo/mocks"
 	"vipulsirdemo/models"
 	v1req "vipulsirdemo/resources/request/api/v1"
-	v1res "vipulsirdemo/resources/response/api/v1"
 )
 
 var (
@@ -16,59 +13,34 @@ var (
 )
 
 
-var OrderRequest = v1req.OrderRequest{
-	ID:          1,
-	UserId:      1,
-	OrderNumber: "001",
-	Description: "Testing Description",
-	Date:        time.Time{},
-	Status:      "active",
-}
-var OrderData = models.Order{
-	Model: models.Model{
-		ID: 1,
-	},
-	UserId:      1,
-	OrderNumber: "123",
-	Description: "Testing description",
-	Status:      "active",
-}
-var OrderResponse = v1res.OrderResponse{
-	OrderNumber: "001110",
-	Description: "testing description",
-	Status:      "active",
-	UserId:      1,
-}
+func Test_AddOrders_MustReturnSuccess(t *testing.T)  {
+	orderRepoMock := new(mocks.OrderRepository)
 
+	var orderRequestParams v1req.OrderRequest
 
-func Test_AddOrders_MustReturnSuccess(t *testing.T) {
-	OrderData := map[string]interface{}{
-		"data": v1req.OrderRequest{
-			ID:   1,
-			UserId:  1,
-			OrderNumber: "1234567890",
-			Description:  "This is the testing",
-			Date: time.Now(),
-			Status: "active",
+	orderRepoMock.On("AddOrder", orderRequestParams).Return(&models.Order{
+		Model: models.Model{
+			ID: 1,
 		},
-		"message": "Order added successfully.",
-		"status":  1,
-	}
-	AddOrderMock := new(mocks.OrderRepository)
-
-	AddOrderMock.On("AddOrder", mock.Anything).Return(OrderData)
+		UserId: 1,
+		Status: "active",
+		Description: "Description for testing",
+		OrderNumber: "011",
+	})
 
 	var srvc OrderService
-	err := srvc.AddOrder(OrderRequest)
-	if err != nil {
-		assert.Equal(t, err, OrderData)
-	}
+	err := srvc.AddOrder(orderRequestParams)
+	assert.NotNil(t, err, nil)
 }
 
-/*func Test_GetOrders_MustReturnSuccess(t *testing.T) {
-	TestGetOrderMock := new(mocks.OrderRepository)
+func Test_AddOrders_Fail(t *testing.T)  {
+	orderRepoMock := new(mocks.OrderRepository)
 
-	TestGetOrderMock.On("GetOrderByUser", mock.Anything).Return(id)
+	var orderRequestParams v1req.OrderRequest
 
+	orderRepoMock.On("AddOrder", orderRequestParams).Return(nil)
+
+	var srvc OrderService
+	err := srvc.AddOrder(orderRequestParams)
+	assert.Nil(t, err, nil)
 }
-*/
